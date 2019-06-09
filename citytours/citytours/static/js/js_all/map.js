6df8ed2f7b2d35a196357311edaeda7f3fef3e00
Bradley Dice <bdice@bradleydice.com>
2019-06-09 11:16:28 -0400

    Add working map.

--- citytours/citytours/static/js/js_all/map.js
+++ citytours/citytours/static/js/js_all/map.js
@@ -1,5 +1,9 @@
-let CartoDB_Voyager = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-  subdomains: 'abcd',
-  maxZoom: 19
+$(document).on('turbolinks:load', function() {
+  let CartoDB_Voyager = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    subdomains: 'abcd',
+    maxZoom: 19
+  });
+  var mymap = L.map('mapid').setView([42.2808, -83.7430], 13);
+  CartoDB_Voyager.addTo(mymap);
 });
